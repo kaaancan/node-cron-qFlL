5520f5d7c8550e1ade62100bd261e08c0dcdffd7
KaanCan <k_goet@outlook.de>
2023-03-14 23:47:54 +0100

    lgtm

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,9 @@
 import cron from 'node-cron';
+import {PrismaClient} from '@prisma/client';
 
+const prisma = new PrismaClient();
 cron.schedule(`*/1 * * * *`, async () => {
+  const some = await prisma.job.findMany();
+  console.log(some);
   console.log(`running your task...`);
 });
